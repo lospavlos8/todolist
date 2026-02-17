54ede660e6cdb84873b4a4d1478009accbefde15
lospavlos8 <sippavel808@gmail.com>
2026-02-17 20:56:11 +0100

    detail a delete

---
+++ src/pages/api/tasks/[id].ts
@@ -0,0 +1,19 @@
+import { promises as fs } from 'fs';
+import path from 'path';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const dataPath = path.join(process.cwd(), 'data', 'tasks.json');
+    const taskId = Number(req.query.id);
+    const fileContents = await fs.readFile(dataPath, 'utf8');
+    let tasks = JSON.parse(fileContents);
+    const taskIndex = tasks.findIndex((t: any) => t.id === taskId);
+
+    if (req.method === 'GET') {
+        res.status(200).json(tasks[taskIndex]);
+    } else if (req.method === 'DELETE') {
+        tasks = tasks.filter((t: any) => t.id !== taskId);
+        await fs.writeFile(dataPath, JSON.stringify(tasks, null, 2));
+        res.status(200).json({ message: 'Smazano' });
+    }
+}
