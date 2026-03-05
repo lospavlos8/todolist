949ec1c229f24f3c7df59c8874904bfb91692d60
lospavlos8 <sippavel808@gmail.com>
2026-03-05 20:43:37 +0100

    GET a POST s Prisma ORM a Zod

--- src/pages/api/tasks/index.ts
+++ src/pages/api/tasks/index.ts
@@ -1,25 +1,38 @@
-import { promises as fs } from 'fs';
-import path from 'path';
+import { PrismaClient } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { taskSchema } from '../../../../utils/validation';
 
 
-type Task = { id: number; title: string; completed: boolean; };
+const prisma = new PrismaClient();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    const dataPath = path.join(process.cwd(), 'data', 'tasks.json');
-    const fileContents = await fs.readFile(dataPath, 'utf8');
-    const tasks: Task[] = JSON.parse(fileContents);
-
     if (req.method === 'GET') {
-        const { finished } = req.query;
-        if (finished === 'true') return res.status(200).json(tasks.filter((t: Task) => t.completed === true));
-        if (finished === 'false') return res.status(200).json(tasks.filter((t: Task) => t.completed === false));
-        return res.status(200).json(tasks);
-    } else if (req.method === 'POST') {
-        if (!req.body.title) return res.status(400).json({ error: 'Chybi nazev' });
-        const newTask: Task = { id: Date.now(), title: req.body.title, completed: false };
-        tasks.push(newTask);
-        await fs.writeFile(dataPath, JSON.stringify(tasks, null, 2));
-        res.status(201).json(newTask);
+        try {
+            const tasks = await prisma.task.findMany();
+            return res.status(200).json(tasks);
+        } catch (error) {
+            return res.status(500).json({ error: "Chyba databáze při načítání úkolů" });
+        }
     }
+
+    if (req.method === 'POST') {
+
+        const validation = taskSchema.safeParse(req.body);
+        if (!validation.success) {
+            return res.status(400).json({ error: validation.error.format() });
+        }
+
+        try {
+            const newTask = await prisma.task.create({
+                data: { title: validation.data.title },
+            });
+            return res.status(201).json(newTask);
+        } catch (error) {
+            return res.status(500).json({ error: "Chyba serveru při ukládání do DB" });
+        }
+    }
+
+
+    res.setHeader('Allow', ['GET', 'POST']);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
 }
